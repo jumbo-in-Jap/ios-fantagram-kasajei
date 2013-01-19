437689c390e4358c9cef7ba53a842daecd9e2966
kasajei@retina <sevenbluesea.yk@gmail.com>
2013-01-19 23:14:03 +0900

    Parameter追加

---
+++ fantagram/fantagram/Parameter.h
@@ -0,0 +1,14 @@
+//
+//  Parameter.h
+//  fantagram
+//
+//  Created by Kasajima Yasuo on 2013/01/19.
+//  Copyright (c) 2013年 kasajei. All rights reserved.
+//
+
+#ifndef fantagram_Parameter_h
+#define fantagram_Parameter_h
+
+
+
+#endif
